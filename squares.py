124e59162c8cbcf5681af3b76da4653874a11c18
ziang <ziangshi34@gmail.com>
2025-11-17 10:13:07 +0000

    Argument Parsing 1/3 #31

--- squares.py
+++ squares.py
@@ -52,12 +52,13 @@ def convert_numbers(list_of_strings):
 
 
 if __name__ == "__main__":
-    numbers_strings = ["1","2","4"]
-    weight_strings = ["1","1","1"]        
-    
-    numbers = convert_numbers(numbers_strings)
-    weights = convert_numbers(weight_strings)
-    
+    import argparse
+    parser = argparse.ArgumentParser(...)
+    parser.add_argument("numbers", nargs="+", type=float, help="...")
+    args = parser.parse_args()
+
+    numbers = args.numbers      
+    weights = None              
+
     result = average_of_squares(numbers, weights)
-    
     print(result)
